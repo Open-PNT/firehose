25fe9477884040c1138cd1e5b7f73b17646e908a
jason <jason.zheng@is4s.com>
2025-06-17 14:03:40 -0400

    use insteadOf function of git to replace ssh URLs with https

--- docker/user_bootstrap.sh
+++ docker/user_bootstrap.sh
@@ -8,11 +8,20 @@ gid="$(ls -nd . | awk '{print $4}')"
 user="docker"
 group="docker"
 
-# Create an account and run as that account.
+# Create an account
 groupadd "$group" --non-unique -g "$gid"
 useradd "$user" --non-unique -m -u "$uid" -g "$gid"
 chown $user:$group /home/$user
 echo "ALL ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 export HOME="/home/$user"
+
+# If in CI, rewrite SSH URLs
+if [[ -n $TOKEN_URL ]]; then
+  echo -e \
+  "[url \"$TOKEN_URL\"]\n\tinsteadOf = git@git.aspn.us:\n\tinsteadOf = ssh://git@git.aspn.us/" \
+  > ~/.gitconfig
+fi
+
+# Run as account created above
 exec runuser -g "$group" -u "$user" -- /bin/bash -c "$scl_args $@"
